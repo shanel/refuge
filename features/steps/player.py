3ef4d168abba3f1146fb605b13a8e3e5c8878aeb
Shane Liebling <shanel@gmail.com>
2019-08-05 21:39:53 -0400

    Got the player creation feature working.

--- features/steps/player.py
+++ features/steps/player.py
@@ -1,18 +1,33 @@
+import requests
+import time
+
 from behave import *
+
+player_name = 'SomePlayer%d' % int(time.time())
 
 @given(u'the player does not currenty exist in the system')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the player does not currenty exist in the system')
+    # Do a GET for /player/SomePlayer and get a 404 (as opposed to no perms - eventaully we'll want auth for this
+    # with a public view and a private one?)
+    url = 'http://localhost:8080/player/%s' % player_name
+    resp = requests.get(url=url)
+    assert resp.status_code == 404, "want 404; got %d" % resp.status_code
 
 
 @when(u'we pass in the requisite player data')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When we pass in the requisite player data')
+    # Do a POST to /player/create
+    url = 'http://localhost:8080/player/create'
+    resp = requests.post(url=url,data={'name': player_name})
+    assert resp.status_code == 200, "want 200; got %d" % resp.status_code
 
 
 @then(u'a barebones player is created.')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then a barebones player is created.')
+    # Do a GET for /player/SomePlayer and get a 200
+    url = 'http://localhost:8080/player/%s' % player_name
+    resp = requests.get(url=url)
+    assert resp.status_code == 200, "want 200; got %d" % resp.status_code
 
 
 @given(u'the player exists in the system')
